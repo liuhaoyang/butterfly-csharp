bff94e7588574c3ae43d6417bfc27e5d35a7231d
Lemon <liuhaoyang1221@hotmail.com>
2017-11-14 23:34:37 +0800

    add AspectCore.APM.RedisProfiler

---
+++ src/AspectCore.APM.RedisProfiler/AspectRedisDatabaseProfiler.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+namespace AspectCore.APM.RedisProfiler
+{
+    public sealed class AspectRedisDatabaseProfiler : IProfiler
+    {
+        public object GetContext()
+        {
+            return AspectRedisDatabaseProfilerContext.Context;
+        }
+    }
+}
