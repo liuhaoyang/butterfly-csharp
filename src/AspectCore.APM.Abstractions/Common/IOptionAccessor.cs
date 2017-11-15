b83731975c590f5e2888aa0bc007368bff508183
Lemon <liuhaoyang1221@hotmail.com>
2017-11-15 23:12:53 +0800

    add IOptionAccessor

---
+++ src/AspectCore.APM.Abstractions/Common/IOptionAccessor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspectCore.APM.Common
+{
+    public interface IOptionAccessor<TOption>
+    {
+        TOption Value { get; set; }
+    }
+}
--- src/AspectCore.APM.LineProtocolCollector/Class1.cs
+++ src/AspectCore.APM.LineProtocolCollector/Class1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace AspectCore.APM.LineProtocolCollector
 {
     public class Class1
     {
+       // InfluxDB.LineProtocol.Client.LineProtocolClient
     }
 }
