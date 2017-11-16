bb5253a687cb68ea8b6acf7f0f6057eb7aeab14d
liuhaoyang <liuhaoyang@taolx.com>
2017-11-16 16:09:33 +0800

    add LineProtocolPayloadClient

---
+++ src/AspectCore.APM.LineProtocolCollector/LineProtocolClientOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspectCore.APM.LineProtocolCollector
+{
+    public class LineProtocolClientOptions
+    {
+        public string ServerAddress { get; set; }
+
+        public string Database { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ src/AspectCore.APM.LineProtocolCollector/LineProtocolPayloadClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using AspectCore.APM.Collector;
+using AspectCore.APM.Common;
+using InfluxDB.LineProtocol.Client;
+using InfluxDB.LineProtocol.Payload;
+
+namespace AspectCore.APM.LineProtocolCollector
+{
+    public class LineProtocolPayloadClient : IPayloadClient
+    {
+        private readonly LineProtocolClient _lineProtocolClient;
+        private readonly LineProtocolClientOptions _lineProtocolClientOptions;
+
+        public LineProtocolPayloadClient(LineProtocolClientOptions lineProtocolClientOptions)
+        {
+            _lineProtocolClientOptions = lineProtocolClientOptions ?? throw new ArgumentNullException(nameof(lineProtocolClientOptions));
+            _lineProtocolClient = new InternalLineProtocolClient(
+                _lineProtocolClientOptions.ServerAddress, _lineProtocolClientOptions.Database,
+                _lineProtocolClientOptions.UserName, _lineProtocolClientOptions.Password);
+        }
+
+        public Task WriteAsync(IPayload payload, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var lineProtocolPayload = new LineProtocolPayload();
+            foreach (var point in payload)
+            {
+                lineProtocolPayload.Add(new LineProtocolPoint(point.Name, point.GetFields(), point.GetTags(), point.Timestamp));
+            }
+            return _lineProtocolClient.WriteAsync(lineProtocolPayload, cancellationToken);
+        }
+
+        private class InternalLineProtocolClient : LineProtocolClient
+        {
+            public InternalLineProtocolClient(string serverBaseAddress, string database, string username = null, string password = null)
+                : base(new HttpClientHandler() { UseProxy = false, Proxy = null }, new Uri(serverBaseAddress), database, username, password)
+            {
+            }
+        }
+    }
+}
