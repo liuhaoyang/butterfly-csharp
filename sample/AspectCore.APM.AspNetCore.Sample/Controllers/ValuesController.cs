3c4fe87155048cf930d7520938a9d5bd7c4b7799
Lemon <liuhaoyang1221@hotmail.com>
2017-11-18 20:58:39 +0800

    add AspectCore.APM.AspNetCore.Sample project

---
+++ sample/AspectCore.APM.AspNetCore.Sample/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspectCore.APM.AspNetCore.Sample.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Values")]
+    public class ValuesController : Controller
+    {
+        public string Get()
+        {
+            return "value";
+        }
+    }
+}
