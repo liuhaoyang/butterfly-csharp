25622fa23b1ba50f89090d5ab51d09c04dd41cd2
Lemon <liuhaoyang1221@hotmail.com>
2017-11-20 15:37:53 +0800

    pack

--- src/AspectCore.APM.AspNetCore/ConfigurationExtensions.cs
+++ src/AspectCore.APM.AspNetCore/ConfigurationExtensions.cs
@@ -5,12 +5,21 @@ using Microsoft.Extensions.Configuration;
 
 namespace AspectCore.APM.AspNetCore
 {
-   public static class ConfigurationExtensions
+    public static class ConfigurationExtensions
     {
         public static IConfigurationSection GetLineProtocolSection(this IConfiguration configuration)
         {
             return configuration.GetSection("AspectCoreAPM:Collector:LineProtocol");
-            
+        }
+
+        public static IConfigurationSection GetApplicationProfilerSection(this IConfiguration configuration)
+        {
+            return configuration.GetSection("AspectCoreAPM:Profiler:Application");
+        }
+
+        public static IConfigurationSection GetRedisProfilerSection(this IConfiguration configuration)
+        {
+            return configuration.GetSection("AspectCoreAPM:Profiler:Redis");
         }
     }
 }
