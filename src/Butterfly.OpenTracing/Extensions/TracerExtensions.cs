a23e444cce7cf0ea1a48949e1aacd53b8e9579ab
lemon <liuhaoyang1221@hotmail.com>
2018-01-02 23:09:26 +0800

    move openTracing to client

---
+++ src/Butterfly.OpenTracing/Extensions/TracerExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Butterfly.OpenTracing.Extensions
+{
+    public static class TracerExtensions
+    {
+        public static ISpan GetCurrentSpan(this ITracer tracer)
+        {
+            return SpanLocal.Current;
+        }
+
+        public static void SetCurrentSpan(this ITracer tracer, ISpan spanContext)
+        {
+            SpanLocal.Current = spanContext;
+        }
+    }
+}
